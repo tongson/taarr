faf2bba6d335189f3a16694b30daa7fae7c0c69b
Eduardo Tongson <propolice@gmail.com>
2021-03-25 17:35:30 +0800

    Release 0.0.7.

--- cmd/rr/main.go
+++ cmd/rr/main.go
@@ -12,8 +12,8 @@ import (
 	"lib"
 )
 
-const versionNumber = "0.0.6"
-const codeName = "\"Spooky Eagle\""
+const versionNumber = "0.0.7"
+const codeName = "\"Helpless Geometry\""
 const (
 	libHeader = `#!/bin/sh
 unset IFS
