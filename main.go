86656d945b9561c84e89eabf993d750869ac50b3
Eduardo Tongson <propolice@gmail.com>
2024-05-01 10:19:38 +0800

    init() is a better place for terminal state storage and signal handling.

--- main.go
+++ main.go
@@ -67,38 +67,33 @@ type optT struct {
 	mode     int
 }
 
-// https://gist.github.com/jlinoff/e8e26b4ffa38d379c7f1891fd174a6d0
-func getPassword(prompt string) (string, error) {
-	var err error
-	// Get the initial state of the terminal.
-	initialTermState, err := terminal.GetState(syscall.Stdin)
-	if err != nil {
-		return "", err
-	}
+var initTermState *terminal.State
 
-	// Restore it in the event of an interrupt.
-	// CITATION: Konstantin Shaposhnikov - https://groups.google.com/forum/#!topic/golang-nuts/kTVAbtee9UA
-	// syscall.SIGTERM according to staticcheck
+// CITATION: Konstantin Shaposhnikov - https://groups.google.com/forum/#!topic/golang-nuts/kTVAbtee9UA
+// REFERENCE: https://gist.github.com/jlinoff/e8e26b4ffa38d379c7f1891fd174a6d0
+func init() {
+	var err error
+	initTermState, err = terminal.GetState(syscall.Stdin)
+	if err != nil {
+			return
+	}
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		_ = terminal.Restore(syscall.Stdin, initialTermState)
-		os.Exit(1)
+		_ = terminal.Restore(syscall.Stdin, initTermState)
+		os.Exit(2)
 	}()
+}
 
-	// Now get the password.
+func getPassword(prompt string) (string, error) {
+	var err error
 	fmt.Print(prompt)
 	p, err := terminal.ReadPassword(syscall.Stdin)
 	fmt.Println("")
 	if err != nil {
 		return "", err
 	}
-
-	// Stop looking for ^C on the channel.
-	signal.Stop(c)
-
-	// Return the password as a string.
 	return string(p), err
 }
 
