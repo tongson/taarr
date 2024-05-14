f8728f967f39449d0b0283a6369a3dd035bd580b
Eduardo Tongson <propolice@gmail.com>
2024-05-14 22:02:11 +0800

    Split constants to its own file.

---
+++ constants.go
@@ -0,0 +1,26 @@
+package main
+
+const versionNumber = "2.0.1"
+const codeName = "\"Hypnotic Antennae\""
+
+const cOP = "OP"
+const cINC = "VARS"
+const cHOSTS = "HOSTS"
+const cLOG = "LOG"
+const cREPAIRED = "__REPAIRED__"
+const cRUN = "script"
+const cINTERP = "shell"
+const cDOC = "readme"
+const cTIME = "02 Jan 06 15:04"
+
+const cSTDOUT = " ┌─ stdout"
+const cSTDERR = " ┌─ stderr"
+const cSTDDBG = " ┌─ debug"
+const cFOOTER = " └─"
+
+const cTARC = "--no-same-owner --no-same-permissions"
+const cTARX = "--no-same-owner --no-same-permissions --no-overwrite-dir --no-acls --no-selinux --no-xattrs --touch"
+
+const oJson int = 0
+const oTerm int = 1
+const oPlain int = 2
